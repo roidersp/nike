9a6c040987619b2469addceb5694ea8378f72da3
Rodrigo Luis Santander <roidersp@gmail.com>
2015-03-27 17:45:24 -0600

    dasdasdas

--- js/indepth.js
+++ js/indepth.js
@@ -1,6 +1,6 @@
 var disqus_shortname = 'juanfutbol';
 var disqus_identifier;
-var disqus_url="badae6-ff03-4e47-88d4-9fc18da1a507";
+var disqus_url="2193d7d6-bf12-4a05-9eb2-201bbed92f83";
 var disqus_number_c=2;
 var disqus_per_page=3;
 var tamaño_total=1920;
